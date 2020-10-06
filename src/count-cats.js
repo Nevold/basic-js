450f299633f0416ba32d708c9fa71fdb0a06d3b7
Nevold <nevold@tut.by>
2020-10-07 01:07:32 +0300

    feat: add function dream, fix function count

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
-  throw new CustomError('Not implemented');
+  // throw new CustomError('Not implemented');
   let number=0
   for(let i=0;i<backyard.length;i++){
    for(let j=0;j<backyard[i].length;j++){
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members) {
+  // throw new CustomError('Not implemented');
+  if(Array.isArray(members) && members.length >0){
+  let count='';
+  for (i=0; i<members.length; i++){
+    if(typeof members[i]=='string' ){
+    count=count+members[i][0];
+    }
+  }
+  return count.toUpperCase().split('').sort().join('');
+} else{
+  return false;
+} 
 };
