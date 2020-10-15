c4c9266497ff0338781bb8bc076766c1479867b8
Nevold <nevold@tut.by>
2020-10-15 17:06:14 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,6 +2,12 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
 
+  if(!Array.isArray(arr)){
+    throw new Error();
+};
+ if(arr.length == 0){
+    return [];
+ };
    
 
   let arrDiscardNext=arr.slice();
