251850fc307cc271a878d7d7b4a5b7a79d1839ff
Nevold <nevold@tut.by>
2020-10-15 23:24:52 +0300

    fix: add recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,13 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth( arr) {
-    let count=1;
+  calculateDepth(arr) {
+    let result = 0;
     for (let i = 0; i < arr.length; i++) {
-      if( Array.isArray(arr[i])){
-        count ++
+      if (Array.isArray(arr[i])) {
+          let countFunc= this.calculateDepth(arr[i])
+        if (countFunc > result) {result = countFunc}
       }
-     }
-     return count;
     }
+    result++;
+    return result;
+  }
 };
